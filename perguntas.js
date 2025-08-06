e76f5743b8f470ea587f2a2da17b656f7b9a0e5c
pablosouza03-netizen <pablo.souza03@escola.pr.gov.br>
2025-08-06 08:16:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,83 @@
+criaCartao(
+    'Esportes',
+    'Qual país sediou a Copa do Mundo de Futebol de 2014?',
+    'Brasil'
+)
+
+criaCartao(
+    'Esportes',
+    'Quantos jogadores compõem um time de basquete em quadra durante uma partida?',
+    'Cinco jogadores'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual é o nome do atleta brasileiro considerado o "Rei do Futebol"?',
+    'Pelé'
+)
+
+criaCartao(
+    'Esportes',
+    'Em qual esporte se utiliza um taco para acertar uma bola em buracos?',
+    'golfe'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual é a modalidade de natação mais rápida?',
+    'Nado livre'
+)
+
+criaCartao(
+    'Esportes',
+    'Onde foram realizados os primeiros Jogos Olímpicos da era moderna?',
+    'Atenas, Grécia'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual é o esporte em que os atletas remam em barcos estreitos chamados de canoas ou caiaques?',
+    'Canoagem'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual tenista é o maior vencedor de Grand Slams masculinos?',
+    'Novak Djokovic'
+)
+
+criaCartao(
+    'Esportes',
+    'Em que esporte o Brasil é conhecido como a maior potência mundial, com diversos títulos olímpicos e mundiais?',
+    'Futebol'
+)
+
+criaCartao(
+    'Esportes',
+    'O que significa "hat-trick" no futebol?',
+    'É quando um jogador marca 3 gols em um jogo'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual esporte é praticado em uma quadra de areia com uma rede no meio e raquetes?',
+    'Tenis, Beatch Tenis'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual é a cor do quimono utilizado pelo judoca com a maior graduação (faixa)?',
+    'Vermelha (ou, mais precisamente, branca com faixas vermelhas) para a 10ª e a 11ª graduação, mas o judoca com a graduação mais comum é a preta.'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual é o nome do esporte em que os atletas lançam um disco de metal o mais longe possível?',
+    'Lançamento de disco'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual equipe é a maior vencedora da história da NBA (National Basketball Association)?',
+    'Bonston Celtics'
+)
